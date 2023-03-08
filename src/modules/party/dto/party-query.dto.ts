0837abf8f5a817b6fec6187e07d8005e1940a55a
oscar-richardson-softwire <oscar.richardson@softwire.com>
2023-03-08 18:22:21 +0000

    refactor(APIM-79): remove IsNotEmpty check from searchText as this is covered by length >= 3 check

--- src/modules/party/dto/party-query.dto.ts
+++ src/modules/party/dto/party-query.dto.ts
@@ -2,8 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, Length } from 'class-validator';
 
 export class PartyQueryDto {
-  @IsNotEmpty()
-  @Length(3)
   @ApiProperty()
+  @Length(3)
   searchText: string;
 }
