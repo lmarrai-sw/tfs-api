d4e9eeea591f1a6bfcb761f7d2562b1fa6a79724
oscar-richardson-softwire <oscar.richardson@softwire.com>
2023-03-08 18:27:34 +0000

    refactor(APIM-79): add info about searchText having a min length of 3 to swagger

--- src/modules/party/dto/party-query.dto.ts
+++ src/modules/party/dto/party-query.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, Length } from 'class-validator';
 
 export class PartyQueryDto {
-  @ApiProperty()
+  @ApiProperty({ description: 'Minimum length: 3', minLength: 3 })
   @Length(3)
   searchText: string;
 }
